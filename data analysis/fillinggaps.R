349ff28ce8c0ac8c41efd14f961e677a4c94070d
Asya Simonyan <anniesim97@gmail.com>
2019-06-02 03:06:04 +0300

    plots

--- data analysis/fillinggaps.R
+++ data analysis/fillinggaps.R
@@ -2,13 +2,10 @@ library(tidyverse)
 library(mlogit)
 library(nnet)
 
-nat <- read.csv('https://raw.githubusercontent.com/acRnR/Referential-Choice-in-Subordinate-Clauses-in-Native-and-L2-Russian-Speakers/master/data/nativeg.csv', sep=';', encoding = 'UTF-8')
+nat <- read.csv('https://raw.githubusercontent.com/acRnR/Referential-Choice-in-Subordinate-Clauses-in-Native-and-L2-Russian-Speakers/master/data/trynew.csv', sep=';', encoding = 'UTF-8')
 
 str(nat)
 names(nat) <- c('conj', 'rd', 'clord', 'choice')
-
-output <- mlogit(choice~1|conj+rd+clord, data=nat)
-summary(output)
 
 rownames(mydata)
 nat$out <- relevel(nat$choice, ref='no')
@@ -24,3 +21,21 @@ print(cm)
 z <- summary(mymodel)$coefficients/summary(mymodel)$standard.errors
 p <- (1 - pnorm(abs(z), 0, 1))*2
 p
+
+write.csv(z, file = "C:/Users/ssimo/Desktop/Diplom/data/native_z.csv")
+write.csv(p, file = "C:/Users/ssimo/Desktop/Diplom/data/native_p.csv")
+
+library(effects)
+plot(Effect("clord", mymodel))
+plot(Effect("clord", mymodel),multiline=T)
+plot(Effect("clord",mymodel),style="stacked") 
+install.packages('effects')
+
+plot(Effect("conj", mymodel))
+plot(Effect("conj", mymodel),multiline=T)
+plot(Effect("conj",mymodel),style="stacked") 
+
+plot(Effect("rd", mymodel))
+plot(Effect("rd", mymodel),multiline=T)
+plot(Effect("rd",mymodel),style="stacked") 
+
